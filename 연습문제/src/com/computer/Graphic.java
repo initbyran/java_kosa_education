a6d7c8c33e78f732dad9863359ff5a5a87a3b079
DESKTOP-FNSHOAL\KOSA <initbyran@naver.com>
2022-12-06 17:25:09 +0900

    승전이가 도와줌

--- 연습문제/src/com/computer/Graphic.java
+++ 연습문제/src/com/computer/Graphic.java
@@ -5,7 +5,7 @@ public interface Graphic {
 	// (생략가능) public-어디서나 접근- static-인스턴스생성 안해도 가능- final -상수
     
 	// method
-	// puclic abstract 생략 가능
+	// puclic abstract 생략 가능dfddfdfdfdfddf
 	double rendering (int size);
 	
 }
